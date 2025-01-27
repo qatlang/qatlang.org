824745dc6bdd1d9cf968c8dac7143579eb0be75d
Aldrin Mathew <admin@aldrinmathew.com>
2025-01-27 14:07:23 +0530

    Removed unnecessary environment vars
    - Removed environment vars that does not need to be public, from the
      Env class

--- models/env.ts
+++ models/env.ts
@@ -1,6 +1,3 @@
 export const Env = {
-	pocketbaseURL: () => process.env['NEXT_PUBLIC_POCKETBASE_URL'] ?? '',
-	confirmationKey: () => process.env['NEXT_PUBLIC_CONFIRMATION_KEY'] ?? '',
-	gitlabEventSecret: () => process.env['NEXT_PUBLIC_GITLAB_EVENT_SECRET'] ?? '',
-	githubEventSecret: () => process.env['NEXT_PUBLIC_GITHUB_EVENT_SECRET'] ?? '',
-}
+	confirmationKey: () => process.env["NEXT_PUBLIC_CONFIRMATION_KEY"] ?? "",
+};
